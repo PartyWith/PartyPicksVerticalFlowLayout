26bc40d8441b2be7290b49ae794e75a1263b762c
Felipe Ricieri <felipericieri@icloud.com>
2017-11-19 12:48:25 -0200

    Repairing App Delegate

---
+++ Pod/FixedHeightCollectionView.swift
@@ -0,0 +1,9 @@
+//
+//  FixedHeightCollectionView.swift
+//  PartyPicksVertical
+//
+//  Created by Felipe Ricieri on 19/11/2017.
+//  Copyright © 2017 PartyWith. All rights reserved.
+//
+
+import Foundation
